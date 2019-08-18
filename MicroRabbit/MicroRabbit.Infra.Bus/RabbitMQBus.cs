67820a5fa2f4bb2c6969ff30c17cf16c271a3fcc
José Marques <marques@outlook.com>
2019-08-18 01:34:15 -0300

    Initial commit of Infrastructure Dependency project.

--- MicroRabbit/MicroRabbit.Infra.Bus/RabbitMQBus.cs
+++ MicroRabbit/MicroRabbit.Infra.Bus/RabbitMQBus.cs
@@ -4,7 +4,6 @@ using MicroRabbit.Domain.Core.Events;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using MediatR;
